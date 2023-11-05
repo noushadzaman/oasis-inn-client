a5ec35f349c7c623a70b79b3765bbef374a66ac6
Noushadozi <noushadozi333@gmail.com>
2023-11-06 01:11:47 +0600

    featured rooms added

---
+++ src/pages/Home/FeaturedRooms.jsx
@@ -0,0 +1,31 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+
+const FeaturedRooms = () => {
+    return (
+        <div className='my-[80px]'>
+            <h1 className='text-black text-center text-[20px] md:text-4xl lg:text-5xl font-semibold ml-10 mb-[15px]'>Featured Premium Rooms</h1>
+            <p className='text-black text-center text-[12px] md:text-xl ml-10 mb-[25px]'>Experience the Pinnacle of Comfort and Elegance</p>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide><img className='max-w-[450px] h-[300px] object-cover' src="https://i.ibb.co/D88RqYg/frames-for-your-heart-Fqqi-Av-Jejto-unsplash-1.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img className='max-w-[450px] h-[300px] object-cover' src="https://i.ibb.co/gDKWnfx/ciudad-maderas-MXb-M1-Nr-Rqt-I-unsplash-1-1.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img className='max-w-[450px] h-[300px] object-cover' src="https://i.ibb.co/D88RqYg/frames-for-your-heart-Fqqi-Av-Jejto-unsplash-1.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img className='max-w-[450px] h-[300px] object-cover' src="https://i.ibb.co/D88RqYg/frames-for-your-heart-Fqqi-Av-Jejto-unsplash-1.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img className='max-w-[450px] h-[300px] object-cover' src="https://i.ibb.co/D88RqYg/frames-for-your-heart-Fqqi-Av-Jejto-unsplash-1.jpg" alt="" /></SwiperSlide>
+            </Swiper>
+        </div>
+    );
+};
+
+export default FeaturedRooms;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner";
+import FeaturedRooms from "./FeaturedRooms";
 
 const Home = () => {
     return (
         <div className="xl:px-[100px]">
             <Banner></Banner>
+            <FeaturedRooms></FeaturedRooms>
         </div>
     );
 };
